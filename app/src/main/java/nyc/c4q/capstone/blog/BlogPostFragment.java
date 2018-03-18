889d74ef63c525160c04d04a2b31a576c66b2276
C4Q <annekochanski@ac.c4q.nyc>
2018-03-18 12:30:29 -0400

    added blog post fragment

--- app/src/main/java/nyc/c4q/capstone/blog/BlogPostFragment.java
+++ app/src/main/java/nyc/c4q/capstone/blog/BlogPostFragment.java
@@ -3,17 +3,35 @@ package nyc.c4q.capstone.blog;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+
+import java.util.List;
 
 import nyc.c4q.capstone.R;
+import nyc.c4q.capstone.models.DBReturnCampaignModel;
+
+import static nyc.c4q.capstone.MainActivity.firebaseDataHelper;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BlogPostFragment extends Fragment {
+public class BlogPostFragment extends Fragment implements ChildEventListener {
+
+    View rootView;
+    private TextView userImage;
+    private TextView blogPost;
+    private RecyclerView recyclerView;
 
 
     public BlogPostFragment() {
@@ -25,7 +43,49 @@ public class BlogPostFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blog_post, container, false);
+        rootView = inflater.inflate(R.layout.fragment_blog_post, container, false);
+        userImage = rootView.findViewById(R.id.blog_post_imageview);
+        blogPost = rootView.findViewById(R.id.blog_post_textview);
+        recyclerView = rootView.findViewById(R.id.blog_post_recyclerView);
+
+        blogPost.setMovementMethod(new ScrollingMovementMethod());
+
+        return rootView;
+
+
     }
 
+    private void loadTextFromList(List<DBReturnCampaignModel> currentCampaignsList) {
+        for (DBReturnCampaignModel DBReturnCampaignModel : currentCampaignsList) {
+            userImage.setText(DBReturnCampaignModel.getImageUrl());
+            blogPost.setText(DBReturnCampaignModel.getIntro() + DBReturnCampaignModel.getBody());
+        }
+
+    }
+
+    @Override
+    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+        loadTextFromList(firebaseDataHelper.getCampaignsList(dataSnapshot));
+
+    }
+
+    @Override
+    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+    }
+
+    @Override
+    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+    }
+
+    @Override
+    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+    }
+
+    @Override
+    public void onCancelled(DatabaseError databaseError) {
+
+    }
 }
